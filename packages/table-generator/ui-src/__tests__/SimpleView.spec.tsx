199522eb61d7f56a106b84b71a686263bc619e04
origami-z <davidcui2@me.com>
2023-03-29 15:03:05 +0100

    Fix test

--- packages/table-generator/ui-src/__tests__/SimpleView.spec.tsx
+++ packages/table-generator/ui-src/__tests__/SimpleView.spec.tsx
@@ -1,22 +1,73 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  fireEvent,
+  RenderResult,
+} from "@testing-library/react";
 import userEvents from "@testing-library/user-event";
 import React from "react";
+import { DEFAULT_TABLE_CONFIG } from "../../shared-src";
 import { ConfigView } from "../view/ConfigView";
 
 describe("ConfigView", () => {
+  let renderResult: RenderResult;
   beforeEach(() => {
     window.parent.postMessage = jest.fn();
-    render(<ConfigView />);
+    renderResult = render(
+      <ConfigView
+        setTableConfig={() => {}}
+        tableConfig={DEFAULT_TABLE_CONFIG}
+        validTableSelected={true}
+      />
+    );
   });
+  afterEach(() => jest.resetAllMocks());
   test("renders title", () => {
     expect(screen.getByText("Table Config")).toBeInTheDocument();
   });
-  test.skip("sends export message to figma when clicking export", async () => {
-    await userEvents.click(screen.getByRole("button", { name: "Export CSV" }));
+  test("create button is disabled by default", () => {
+    expect(screen.getByRole("button", { name: "Create" })).toHaveAttribute(
+      "aria-disabled",
+      "true"
+    );
+  });
+  test("sends create message to figma when clicking create", async () => {
+    renderResult?.rerender(
+      <ConfigView
+        setTableConfig={() => {}}
+        tableConfig={{
+          ...DEFAULT_TABLE_CONFIG,
+          headerCell: {
+            name: "Test header",
+            key: "abc",
+          },
+          bodyCell: {
+            name: "Test cell",
+            key: "def",
+          },
+        }}
+        validTableSelected={true}
+      />
+    );
+    await userEvents.click(screen.getByRole("button", { name: "Create" }));
     expect(window.parent.postMessage).toHaveBeenCalledTimes(1);
     expect(window.parent.postMessage).toHaveBeenCalledWith(
       expect.objectContaining({
-        pluginMessage: { type: "export-csv-file" },
+        pluginMessage: {
+          type: "generate-table",
+          config: {
+            rows: 3,
+            columns: 3,
+            headerCell: {
+              name: "Test header",
+              key: "abc",
+            },
+            bodyCell: {
+              name: "Test cell",
+              key: "def",
+            },
+          },
+        },
       }),
       "*"
     );
