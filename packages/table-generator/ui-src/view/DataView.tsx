7c70df77cd5f594244d4d4ddfd680b72e5074915
origami-z <davidcui2@me.com>
2023-06-09 13:55:00 +0100

    Disable editable header

--- packages/table-generator/ui-src/view/DataView.tsx
+++ packages/table-generator/ui-src/view/DataView.tsx
@@ -10,24 +10,17 @@ import {
   CellEditor,
   Grid,
   GridColumn,
-  GridHeaderValueProps,
   TextCellEditor,
 } from "@salt-ds/data-grid";
-import { Input } from "@salt-ds/lab";
 import React, { useCallback, useEffect, useState } from "react";
 import { PostToFigmaMessage, PostToUIMessage } from "../../shared-src";
+import {
+  CustomEditableHeader,
+  CustomGridContext,
+} from "../components/GridCustomization";
 import { ViewSharedProps } from "./types";
 
 import "./DataView.css";
-
-const CustomEditableHeader = (props: GridHeaderValueProps<any>) => {
-  const { column } = props;
-  return (
-    <div>
-      <Input defaultValue={column.info.props.name} />
-    </div>
-  );
-};
 
 export const DataView = ({
   onToggleView,
@@ -95,50 +88,63 @@ export const DataView = ({
     );
   };
 
-  console.log({ bodyValues });
+  const onUpdateHeaderValue = (newValue: string, colIndex: number) => {
+    setHeaderValues((prev) => [
+      ...prev.slice(0, colIndex),
+      newValue,
+      ...prev.slice(colIndex + 1),
+    ]);
+  };
 
   return (
     <StackLayout className="data-view" align="stretch" gap={0}>
-      <H2>Data</H2>
+      {/* <H2>Data</H2> */}
       <FlexItem grow={1} shrink={1} style={{ overflow: "auto" }}>
-        <Grid
-          rowData={bodyValues}
-          style={{
-            height: "100%",
+        <CustomGridContext.Provider
+          value={{
+            rows: bodyValues,
+            onUpdateHeaderValue,
           }}
         >
-          {headerValues.map((header, colIndex) => {
-            const id = `${header}-col-${colIndex}`;
-            return (
-              <GridColumn
-                key={id}
-                name={header}
-                id={id}
-                defaultWidth={100}
-                getValue={(x) => x[colIndex]}
-                // TODO: editable header
-                // headerValueComponent={CustomEditableHeader}
-                onChange={(row: string[], rowIndex, newValue) => {
-                  console.log({ row, rowIndex, colIndex, newValue });
-                  const newRowData = [
-                    ...row.slice(0, colIndex),
-                    newValue,
-                    ...row.slice(colIndex + 1),
-                  ];
-                  setBodyValues((prevBodyValues) => [
-                    ...prevBodyValues.slice(0, rowIndex),
-                    newRowData,
-                    ...prevBodyValues.slice(rowIndex + 1),
-                  ]);
-                }}
-              >
-                <CellEditor>
-                  <TextCellEditor />
-                </CellEditor>
-              </GridColumn>
-            );
-          })}
-        </Grid>
+          <Grid
+            rowData={bodyValues}
+            style={{
+              height: "100%",
+            }}
+          >
+            {headerValues.map((header, colIndex) => {
+              const id = `col-${colIndex}`;
+              return (
+                <GridColumn
+                  key={id}
+                  name={header}
+                  id={id}
+                  defaultWidth={100}
+                  getValue={(x) => x[colIndex]}
+                  // TODO: Salt Grid intercepts keyboard events so custom input doesn't work in header
+                  // headerValueComponent={CustomEditableHeader}
+                  onChange={(row: string[], rowIndex, newValue) => {
+                    console.log({ row, rowIndex, colIndex, newValue });
+                    const newRowData = [
+                      ...row.slice(0, colIndex),
+                      newValue,
+                      ...row.slice(colIndex + 1),
+                    ];
+                    setBodyValues((prevBodyValues) => [
+                      ...prevBodyValues.slice(0, rowIndex),
+                      newRowData,
+                      ...prevBodyValues.slice(rowIndex + 1),
+                    ]);
+                  }}
+                >
+                  <CellEditor>
+                    <TextCellEditor />
+                  </CellEditor>
+                </GridColumn>
+              );
+            })}
+          </Grid>
+        </CustomGridContext.Provider>
       </FlexItem>
       <FlexLayout justify="space-between" className="button-bar">
         <Button variant="primary" onClick={onToggleView}>
